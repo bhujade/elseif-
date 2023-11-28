ac0582049f84b310c2e1a79e7bb01c25296f9e77
bhujade <mayurbhujade1@gmail.com.com>
2023-11-28 18:55:15 +0530

    check2

---
+++ elseif1.js
@@ -0,0 +1,12 @@
+
+a= 1;
+ 
+if(a<2){
+    console.log(" a is less than 2");
+}else if(a<5){
+     console.log("a is less than 5");
+}else if(a<10){
+    console.log("a is less than 10");
+}else if(a>10) {
+    console.log("a is greater than 10");
+}
